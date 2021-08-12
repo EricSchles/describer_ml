927795f2a11ebad898619f700cf341c957f99fc5
Eric Schles <ericschles@gmail.com>
2021-08-12 14:19:15 -0400

    why was this a py file?

--- clean_artifacts.sh
+++ clean_artifacts.sh
@@ -1,4 +1,4 @@
 sudo rm -Rf build/
 sudo rm -Rf dist/
 sudo rm -Rf describer_ml.egg-info/
-sudo rm -Rf /usr/local/lib/python3.6/dist-packages/describer_ml*
+#sudo rm -Rf /usr/local/lib/python3.6/dist-packages/describer_ml*
